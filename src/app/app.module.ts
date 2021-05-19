c4cfae3dd9864dcbadb05a4d3fca618a2ddc03f2
RTPablocs <conejerosorianopablo@gmail.com>
2021-05-19 18:58:04 +0200

    Imported FontAwesome, Navbar, Slider, Home, Searchbar, Categories, FormsModule, Shop, SearchPipe and Cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchbarComponent } from './components/searchbar/searchbar.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import {FormsModule} from '@angular/forms';
+import { ShopComponent } from './pages/shop/shop.component';
+import { SearchPipe } from './pipes/search.pipe';
+import { CardsComponent } from './components/cards/cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SliderComponent,
+    HomeComponent,
+    SearchbarComponent,
+    CategoriesComponent,
+    ShopComponent,
+    SearchPipe,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FontAwesomeModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
