68cdedb7bbfaec45d5f54c69a3d3513c64e8d5c2
RTPablocs <conejerosorianopablo@gmail.com>
2021-05-31 17:14:17 +0200

    Added pagination(methods, styles and pipes)

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -9,9 +9,31 @@ export class CardsComponent implements OnInit {
   @Input() Data: any;
   @Input() search: any;
   @Input() filters: any;
-  constructor() { }
+  page = 1;
+
+  totalItems = () => {
+    if (!this.search && !this.filters) {
+      this.page = 1;
+      return this.Data.length;
+    } else if (this.search && !this.filters) {
+      this.page = 1;
+      return this.search.length;
+    } else if (!this.search && this.filters) {
+      this.page = 1;
+      return this.filters.length;
+    } else if (this.search && this.filters){
+      this.page = 1;
+      return this.filters.length;
+    }
+  }
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  getPage(page: number): void {
+    this.page = page;
+  }
 }
