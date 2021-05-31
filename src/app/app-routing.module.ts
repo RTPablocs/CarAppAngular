d4b3490d03fcba673cbf829b7e0d80791e1b1956
RTPablocs <conejerosorianopablo@gmail.com>
2021-05-31 10:46:18 +0200

    Added login and profile routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
 import {ShopComponent} from './pages/shop/shop.component';
+import {LoginComponent} from './pages/login/login.component';
+import {ProfileComponent} from './pages/profile/profile.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'shop', component: ShopComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'profile', component: ProfileComponent},
   {path: '', redirectTo: 'home', pathMatch: 'full'}
 ];
 
