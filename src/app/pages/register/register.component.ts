0ae36670523367b025f22539d5f9740a303a7a50
RTPablocs <conejerosorianopablo@gmail.com>
2021-06-03 17:46:40 +0200

    Added formgroup and generateSubstring method

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerData = new FormGroup({
+    username: new FormControl(null),
+    email: new FormControl(null),
+    password: new FormControl(null),
+    confirmPassword: new FormControl(null)
+  });
+  name!: string;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  generateSubstring(): void {
+    const base = this.registerData.value.username;
+    this.name = base.substring(0, 2);
+  }
 }
