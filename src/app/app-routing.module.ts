a23d1dfa31b4bbd2aae6cbf533741afd4905e2e5
RTPablocs <conejerosorianopablo@gmail.com>
2021-06-03 16:49:42 +0200

    Added AuthGuard to /profile and register route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import {HomeComponent} from './pages/home/home.component';
 import {ShopComponent} from './pages/shop/shop.component';
 import {LoginComponent} from './pages/login/login.component';
 import {ProfileComponent} from './pages/profile/profile.component';
+import {RegisterComponent} from './pages/register/register.component';
+import {AuthGuard} from './guards/auth.guard';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'shop', component: ShopComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'profile', component: ProfileComponent},
+  {path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]},
+  {path: 'register', component: RegisterComponent},
   {path: '', redirectTo: 'home', pathMatch: 'full'}
 ];
 
