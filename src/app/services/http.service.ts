274986db2fa3bd794cb66785a24d7bd033f020e8
RTPablocs <conejerosorianopablo@gmail.com>
2021-06-08 23:40:53 +0200

    Added functions to retrieve data from server

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  getAllProducts(): any {
+    return this.http.get('http://localhost:8051/products');
+  }
+
+  getSingleProduct(ProductUid: string): any {
+    const url = 'http://localhost:8051/product/' + ProductUid;
+    return this.http.get(url);
+  }
+
 }
