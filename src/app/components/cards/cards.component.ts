1eedec6e14d6a15e7ad4019057a266a5ad7db67b
RTPablocs <conejerosorianopablo@gmail.com>
2021-06-08 23:37:34 +0200

    Imported Icons from faIcons, added onChanges and changed totalItems function

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,11 +1,21 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, OnChanges, SimpleChanges} from '@angular/core';
+import {faTag} from '@fortawesome/free-solid-svg-icons/faTag';
+import {faTachometerAlt} from '@fortawesome/free-solid-svg-icons/faTachometerAlt';
+import {faCar} from '@fortawesome/free-solid-svg-icons/faCar';
+import {faEye} from '@fortawesome/free-solid-svg-icons/faEye';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.css']
 })
-export class CardsComponent implements OnInit {
+export class CardsComponent implements OnInit, OnChanges{
+
+  faTag = faTag;
+  faTachometerAlt = faTachometerAlt;
+  faCar = faCar;
+  faEye = faEye;
+
   @Input() Data: any;
   @Input() search: any;
   @Input() filters: any;
@@ -13,21 +23,21 @@ export class CardsComponent implements OnInit {
 
   totalItems = () => {
     if (!this.search && !this.filters) {
-      this.page = 1;
       return this.Data.length;
     } else if (this.search && !this.filters) {
-      this.page = 1;
       return this.search.length;
     } else if (!this.search && this.filters) {
-      this.page = 1;
       return this.filters.length;
-    } else if (this.search && this.filters){
-      this.page = 1;
+    } else if (this.search && this.filters) {
       return this.filters.length;
     }
   }
 
   constructor() {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.page = 1;
   }
 
   ngOnInit(): void {
