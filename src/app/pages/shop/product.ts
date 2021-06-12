16d363a844f1fb399f77ec7ed8ac91b457b2f19d
RTPablocs <conejerosorianopablo@gmail.com>
2021-06-13 01:44:47 +0200

    Interface for Products Array

---
+++ src/app/pages/shop/product.ts
@@ -0,0 +1,12 @@
+export interface Product {
+  product_uid: string;
+  product_title: string;
+  product_desc: string;
+  product_brand: string;
+  product_model: string;
+  product_category: string;
+  product_price: number;
+  product_miles: number;
+  product_views: number;
+  product_owner: number;
+}
