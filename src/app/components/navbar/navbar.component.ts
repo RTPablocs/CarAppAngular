155ce510d50fb3c4f196bd626082a4bc71d5cd6b
RTPablocs <conejerosorianopablo@gmail.com>
2021-06-13 01:44:32 +0200

    Added cartCurrentQuantity and loggedObservable form CartDataService

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../services/auth.service';
+import {CartDataService} from '../../services/cart-data.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,16 +9,19 @@ import {AuthService} from '../../services/auth.service';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(private auth: AuthService) {
+  constructor(private auth: AuthService, public cart: CartDataService) {
   }
 
-  isLogged = this.auth.isLogged();
+  isLogged = false;
+  cartQuantity = 0;
 
   ngOnInit(): void {
+    this.cart.currentQuantity.subscribe(value => this.cartQuantity = value);
+    this.auth.loggedObservable.subscribe(value => this.isLogged = value);
   }
 
   executeLogout(): void {
-    this.isLogged = this.auth.isLogged();
+    this.auth.loggedIn.next(false);
     this.auth.getLogout();
   }
 }
