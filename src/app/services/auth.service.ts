8a8b1d72efa61c6e229ba10b8174980548a88813
RTPablocs <conejerosorianopablo@gmail.com>
2021-06-13 01:39:00 +0200

    Styles updated

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,19 +1,28 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Router} from '@angular/router';
+import {BehaviorSubject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  loggedIn = new BehaviorSubject(false);
+  loggedObservable = this.loggedIn.asObservable();
+
   constructor(private http: HttpClient, private router: Router) {
   }
 
   getLogin(email: string, password: string): void {
-    this.http.post('http://localhost:5000/api/authenticate', {email: email, password: password})
+    const body = {
+      email,
+      password
+    };
+    this.http.post('http://localhost:8051/login', body)
       .subscribe((resp: any) => {
-        localStorage.setItem('auth', resp.token);
+        this.loggedIn.next(true);
+        localStorage.setItem('auth', resp.result.token);
         this.router.navigate(['profile']);
       });
   }
@@ -24,5 +33,13 @@ export class AuthService {
 
   isLogged(): boolean {
     return localStorage.getItem('auth') !== null;
+  }
+
+  registerUser(body: object): void {
+    this.http.post('http://localhost:8051/register', body)
+      .subscribe((resp: any) => {
+          this.router.navigate(['home']);
+        }
+      );
   }
 }
