626bc2774cdd52678defe87f52df1fc86b67c44d
RTPablocs <conejerosorianopablo@gmail.com>
2021-06-17 11:51:52 +0200

    Added onInit event to get data from localstorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from './services/auth.service';
+import {CartDataService} from './services/cart-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'CarAppFrontend';
+
+  constructor(private auth: AuthService, private cart: CartDataService) {
+  }
+
+  ngOnInit(): void {
+    this.auth.loggedIn.next(this.auth.isLogged())
+    if (localStorage.getItem('cartItems') !== null && localStorage.getItem('cartItemsShow') !== null){
+      this.cart.cartItems = JSON.parse(localStorage.cartItems)
+      this.cart.showArray.next(JSON.parse(localStorage.cartItemsShow))
+      this.cart.cartQuantity.next(this.cart.cartItems.length)
+    }
+  }
 }
