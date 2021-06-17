88d5f6b5bd3d6bf8c3f61767646891c312a86fc9
RTPablocs <conejerosorianopablo@gmail.com>
2021-06-17 11:52:38 +0200

    Added Social Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { SliderComponent } from './components/slider/slider.component';
-import { HomeComponent } from './pages/home/home.component';
-import { SearchbarComponent } from './components/searchbar/searchbar.component';
-import { CategoriesComponent } from './components/categories/categories.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FontAwesomeModule, FaIconLibrary} from '@fortawesome/angular-fontawesome';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {SliderComponent} from './components/slider/slider.component';
+import {HomeComponent} from './pages/home/home.component';
+import {SearchbarComponent} from './components/searchbar/searchbar.component';
+import {CategoriesComponent} from './components/categories/categories.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { ShopComponent } from './pages/shop/shop.component';
-import { SearchPipe } from './pipes/search.pipe';
-import { CardsComponent } from './components/cards/cards.component';
-import { FilteringPipe } from './pipes/filtering.pipe';
-import { FiltersComponent } from './components/filters/filters.component';
+import {ShopComponent} from './pages/shop/shop.component';
+import {SearchPipe} from './pipes/search.pipe';
+import {CardsComponent} from './components/cards/cards.component';
+import {FilteringPipe} from './pipes/filtering.pipe';
+import {FiltersComponent} from './components/filters/filters.component';
 import {NgxPaginationModule} from 'ngx-pagination';
-import { LoginComponent } from './pages/login/login.component';
-import { HttpClientModule} from '@angular/common/http';
-import { ProfileComponent } from './pages/profile/profile.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { AvatarsComponent } from './components/avatars/avatars.component';
-import { DetailsComponent } from './pages/details/details.component';
-import { CartComponent } from './pages/cart/cart.component';
-import {CartDataService} from './services/cart-data.service';
+import {LoginComponent} from './pages/login/login.component';
+import {HttpClientModule} from '@angular/common/http';
+import {ProfileComponent} from './pages/profile/profile.component';
+import {RegisterComponent} from './pages/register/register.component';
+import {AvatarsComponent} from './components/avatars/avatars.component';
+import {DetailsComponent} from './pages/details/details.component';
+import {CartComponent} from './pages/cart/cart.component';
+import {SocialLoginModule, SocialAuthServiceConfig} from 'angularx-social-login';
+import {GoogleLoginProvider} from 'angularx-social-login';
 
 @NgModule({
   declarations: [
@@ -44,16 +45,29 @@ import {CartDataService} from './services/cart-data.service';
     DetailsComponent,
     CartComponent
   ],
-    imports: [
-        BrowserModule,
-        FontAwesomeModule,
-        AppRoutingModule,
-        FormsModule,
-        ReactiveFormsModule,
-        NgxPaginationModule,
-        HttpClientModule
-    ],
-  providers: [CartDataService],
+  imports: [
+    BrowserModule,
+    FontAwesomeModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxPaginationModule,
+    HttpClientModule,
+    SocialLoginModule
+  ],
+  providers: [{
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: false,
+      providers: [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider('620087546049-unjkulomkgkh9ttcjpbp4dcvglmlag0p.apps.googleusercontent.com')
+        }
+      ]
+    } as SocialAuthServiceConfig
+  }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,8 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Router} from '@angular/router';
 import {BehaviorSubject} from 'rxjs';
+import {GoogleLoginProvider, SocialAuthService, SocialUser} from 'angularx-social-login';
+import {User} from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +12,9 @@ export class AuthService {
 
   loggedIn = new BehaviorSubject(false);
   loggedObservable = this.loggedIn.asObservable();
+  socialUser = new SocialUser();
 
-  constructor(private http: HttpClient, private router: Router) {
+  constructor(private http: HttpClient, private router: Router, public socialAuth: SocialAuthService) {
   }
 
   getLogin(email: string, password: string): void {
@@ -22,13 +25,18 @@ export class AuthService {
     this.http.post('http://localhost:8051/login', body)
       .subscribe((resp: any) => {
         this.loggedIn.next(true);
-        localStorage.setItem('auth', resp.result.token);
+        localStorage.setItem('auth', resp.result);
         this.router.navigate(['profile']);
       });
   }
 
+  getUserInfo(): void {
+  }
+
+
   getLogout(): void {
     localStorage.removeItem('auth');
+    this.router.navigate(['/']);
   }
 
   isLogged(): boolean {
@@ -41,5 +49,15 @@ export class AuthService {
           this.router.navigate(['home']);
         }
       );
+  }
+
+  getGoogleLogin(): void {
+    this.socialAuth.signIn(GoogleLoginProvider.PROVIDER_ID)
+      .then(res => {
+        localStorage.setItem('auth', res.authToken);
+        this.socialUser = res;
+        this.loggedIn.next(true);
+        this.router.navigate(['profile']);
+      });
   }
 }
